eab385632ed3a7e18f75d06988e9952ac6449368
Shawn Schwartz <shawnschwartz@ucla.edu>
2019-02-26 20:57:30 -0800

    Added fruitPreprocessing script function

---
+++ AlexNet_experiment/fruitPreprocessing.m
@@ -0,0 +1,19 @@
+function [imdsTrain, imdsTest, all_images] = fruitPreprocessing(image_dir_path)
+%This function loads in each zip file of fruit images to preprocess
+% and outputs each train and test labels as an augumentedImage.
+
+%% Load In and Unpack All Images
+all_zips = dir([image_dir_path '/*.zip']);
+all_zips_paths = {length(all_zips)};
+all_images = {length(all_zips)};
+
+for i = 1:length(all_zips)
+    all_zips_paths{i} = [all_zips(i).folder() '/' all_zips(i).name()];
+    all_images{i} = unzip(all_zips_paths{i});
+    
+    % Resize Images to Match Pretrained Network Input Size
+    imds{i} = imageDatastore(all_images{i}, 'IncludeSubfolders', true, ...
+        'LabelSource', 'foldernames');
+    [imdsTrain, imdsTest] = splitEachLabel(imds, 0.7, 'randomized'); % 70 percent data set for training set
+end
+
