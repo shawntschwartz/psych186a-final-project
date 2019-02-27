8def4c9bbfc0662f9b4cc467b18711b26795831d
Shawn Schwartz <shawnschwartz@ucla.edu>
2019-02-26 20:50:57 -0800

    Added coreFruitNet matlab script with todo for analyses

---
+++ AlexNet_experiment/coreFruitNet.m
@@ -0,0 +1,13 @@
+%% Setup Dir Path for Fruit Images
+main_fruit_path = '_curated-fruit-images';
+
+%% Run Fruit Image Preprocessing
+[fruitFeaturesTrain, fruitFeauturesTest, all_fruit_images] = ...
+    fruitPreprocessing(main_fruit_path);
+
+%% Run Fruit Image Classification and Feature Output Layers
+[fruitFeaturesTrain, fruitFeauturesTest] = ...
+    fruitClassification(fruitFeaturesTrain, fruitFeauturesTest, all_fruit_images);
+
+%% Run Fruit Image Analyses
+% TODO ^^
