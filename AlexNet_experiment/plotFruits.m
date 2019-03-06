04f060018fd692bd3f1c4c66ffd0be61e7fe7c15
Shawn Schwartz <shawnschwartz@ucla.edu>
2019-03-05 17:33:16 -0800

    plotFruit modifications and new output plot

--- AlexNet_experiment/plotFruits.m
+++ AlexNet_experiment/plotFruits.m
@@ -11,13 +11,29 @@ end
 d_test = pdist(fruitTest);
 [Y_test, eigvals_test] = mdscale(d_test, 2);
 
-%% Cast Categorical Labels to Char
+%% Cast Datatype Categorical Labels to Datatype Char
 plot_labs = char(plot_labs);
 
 %% Plot MDS of Image Classified Data
 figure('name', features_data);
 plot(Y_test(:,1), Y_test(:,2), '.');
-
-% TODO: Fix labs
 text(Y_test(:,1)+0.01, Y_test(:,2), plot_labs);
 axis auto square;
+
+%% Save figure as png to _output directory
+% check if directory already exists, if not, create it
+if ~exist(output_dir_path, 'dir')
+    mkdir(output_dir_path);
+end
+
+% retrieve current file name
+[~,name,ext] = fileparts(features_data);
+current_file_name = [name ext];
+
+% now write figure to output dir
+fig_output_filename = [pwd '/' output_dir current_file_name '_figure.png'];
+saveas(gcf, fig_output_filename);
+
+%% Save list of fruit subcategories to txt file
+subcat_output_filename = [pwd '/' output_dir current_file_name '_subcategories.txt'];
+dlmwrite(subcat_output_filename, plot_labs, '');
